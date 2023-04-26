7a31efa7a8f47d267493b316f10d53de69343744
Diego Noronha <diego.noronha@bemobi.com>
2023-04-26 09:04:38 -0400

    feat: add valid_username for model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
          :trackable
 
   has_one_attached :avatar
+
+  validates :full_name, length: { maximum: 50 }
+  validates :body, length: { maximum: 80 }
+  validate :valid_username
+
+  def valid_username
+    errors.add(:username, 'is already taken') if User.exists?(username:)
+
+    restricted_username_list = %(admin root dashboard analytics appearance settings preferences calendar)
+
+    errors.add(:username, 'is restricted') if restricted_username_list.include?(username)
+  end
 end
