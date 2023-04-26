12c9b4a0becda78a94a483211da238b58c7fc947
Diego Noronha <diego.noronha@bemobi.com>
2023-04-26 12:13:28 -0400

    feat: add user create default links

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,12 @@ class User < ApplicationRecord
          :trackable
 
   has_one_attached :avatar
+  has_many :links, dependent: :destroy
+
   friendly_id :username, use: %i[slugged history]
+
+  after_create :create_default_links
+  after_update :create_default_links
 
   validates :full_name, length: { maximum: 50 }
   validates :body, length: { maximum: 80 }
@@ -25,5 +30,11 @@ class User < ApplicationRecord
 
   def should_generate_new_friendly_id?
     username_changed? || slug.blank?
+  end
+
+  private
+
+  def create_default_links
+    Link.create(user: self, title: '', url: '') while links.count < 5
   end
 end
