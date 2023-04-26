73ea260228979cda9b341e0f2f40742d3f3c562a
Diego Noronha <diego.noronha@bemobi.com>
2023-04-26 12:14:35 -0400

    feat: add route patch links update

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   root 'dashboard#index'
   get 'dashboard', to: 'dashboard#index'
   get 'appearance', to: 'dashboard#appearance'
-
   get ':id', to: 'dashboard#show', as: :user
+  patch 'links/:id' => 'links#update', as: :links
 end
