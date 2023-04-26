c573535acf2c33320ad13f0444c24e95b28f792a
Diego Noronha <diego.noronha@bemobi.com>
2023-04-26 09:04:25 -0400

    feat: add update registration controller and news fields to user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -20,9 +20,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  # We need to use a copy of the resource because we don't want to change
+  # the current user in place.
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      set_flash_message_for_update(resource, prev_unconfirmed_email)
+      bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
+
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      redirect_to request.referer, alert: resource.errors.full_messages.join(', ')
+    end
+  end
 
   # DELETE /resource
   # def destroy
@@ -40,9 +56,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
 
+  # https://stackoverflow.com/questions/5113248/devise-update-user-without-password
+  # https://www.mnishiguchi.com/2017/11/24/rails-devise-edit-account-without-password/
   def update_resource(resource, params)
+    # Require current password if user is trying to change password.
     return super if params['password']&.present?
 
+    # Allows user to update registration information without password.
     resource.update_without_password(params.except('current_password'))
   end
 
@@ -51,13 +71,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[avatar full_name username body])
   end
 
   # The path used after sign up.
