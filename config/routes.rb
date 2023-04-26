c348f974badbdf3309c7b72c7ce559d31383f90a
Diego Noronha <diego.noronha@bemobi.com>
2023-04-25 22:03:34 -0400

    feat: add routes get for appearance

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   root 'dashboard#index'
   get 'dashboard', to: 'dashboard#index'
-
-  devise_for :users
+  get 'appearance', to: 'dashboard#appearance'
 
   get ':id', to: 'dashboard#show', as: :user
 end
